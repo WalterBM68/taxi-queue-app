00cd4652bef901e41fc92c10714c1d3c0e3b6c2b
WalterBM68 <Wbmanjanja@gmail.com>
2022-05-30 14:53:00 +0200

    initial commit

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,11 +1,38 @@
-// write your DOM code here.
-
-
-// DOM element references
+// Passengers
+const joinQueueBtn = document.querySelector('.join_queue');
+const leaveQueueBtn = document.querySelector('.leave_queue');
+const countingPassengers = document.querySelector('.passenger_queue_count');
+// Taxi
+const taxiQueueCountBtn = document.querySelector('.join_taxi_queue');
+const countingTaxis = document.querySelector('.taxi_queue_count');
+// Departure
+const departureOfATaxi = document.querySelector('.depart');
 
 // create Factory Function instance
-
 const taxiQueue = TaxiQueue();
 
+let theCount = localStorage.getItem('passengers') ? Number(localStorage.getItem('passengers')) : 0;
+countingPassengers.innerHTML = theCount;
 // DOM events
+joinQueueBtn.addEventListener('click', function(){
+    taxiQueue.joinQueue();
+    countingPassengers.innerHTML = taxiQueue.queueLength();
 
+    localStorage.setItem('passengers', taxiQueue.queueLength());
+});
+
+leaveQueueBtn.addEventListener('click', function(){
+    taxiQueue.leaveQueue();
+    countingPassengers.innerHTML = taxiQueue.queueLength();
+
+    localStorage.setItem('passengers', taxiQueue.queueLength());
+});
+
+taxiQueueCountBtn.addEventListener('click', function(){
+    taxiQueue.joinTaxiQueue();
+    countingTaxis.innerHTML = taxiQueue.taxiQueueLength();
+});
+
+departureOfATaxi.addEventListener('click', function(){
+   taxiQueue.taxiDepart();
+});
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,37 @@
 function TaxiQueue() {
 
+	let passengerCount = 0;
+	let taxiCount = 0;
 
 	function joinQueue() {
-
+        passengerCount++;
 	}
 
 	function leaveQueue() {
-
+		if(passengerCount > 0)
+		    passengerCount--;
 	}
 
 	function joinTaxiQueue() {
-
+        taxiCount++;
 	}
 
 	function queueLength() {
-
+		return passengerCount;
 	}
 
 	function taxiQueueLength() {
-
+		return taxiCount;
 	}
 
 	function taxiDepart(){
-
+       if(passengerCount >= 12){
+		   if(taxiCount > 0){
+			passengerCount-=12;
+			taxiCount--;
+		   }  
+	   }	
+	   taxiQueueLength();
 	}
 
 	return {
